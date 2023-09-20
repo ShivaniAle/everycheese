ed5efa767082f55601425c193fc9e8069f1b932a
ShivaniAle <shivani.Ale@unh.edu>
2023-09-19 22:14:06 -0400

    Implement cheese list page, add navbar link

--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from .models import Cheese
+
+class CheeseListView(ListView):
+    model = Cheese
 
 # Create your views here.
